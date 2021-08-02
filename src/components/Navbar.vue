ab0822bba90ef20b97775e22654668a0bf7f470a
Brian-mula <mulatiwangilabrian@gmail.com>
2021-08-03 00:13:58 +0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <v-toolbar color="deep-purple accent-4" dense dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu left bottom> </v-menu>
+    </v-toolbar>
+
+    <v-navigation-drawer app expand-on-hover v-model="drawer">
+      <v-list>
+        <v-list-item class="px-12">
+          <v-list-item-avatar>
+            <v-img
+              src="https://cdn.pixabay.com/photo/2017/05/31/04/59/beautiful-2359121__340.jpg"
+            />
+          </v-list-item-avatar>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6">
+              Sandra Adams
+            </v-list-item-title>
+            <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
